a4a14f3a34a41627078d5d112b757e720d5a033b
Taylor Somers <tsomers@yandex.com>
2020-06-17 18:06:20 -0400

    Add HTML and UI logic to create two players with independent running total scores
    Co-Authored By: Beverly Potts <pottsbeverly@gmail.com>

--- README.md
+++ README.md
@@ -40,7 +40,7 @@
   | Program will update total score by adding the sum of all rolls within a player's turn when the "Hold" button is pressed. | "Roll: 4, Turn Total: 4; Roll: 3, Turn Total: 7" | "Total Score: 0; Total Score: 0; Total Score: 7" |
   | Program will change the turn total to 0 when the "Hold" button is pushed. | "Roll: 5; Hold" | "Turn Total: 5; Turn Total: 0" |
   | Program will announce the winner when/if one of the players clicks the "Hold" button such that their total score reaches 100 or more. | "Turn Total: 12, Total Score: 95, Hold" | "Alert: 'Player 1 is the winner!'" |
-  | Program will track two parallel sums of randomly generated numbers, which sums are the scores of the respective players. |  |  |
+  | Program will track two parallel sums of randomly generated numbers, which sums are the scores of the respective players. | "Player 1: Roll 5, Turn Total 5 > Hold; Player 2: Roll 4, Turn Total 4 > Hold" | "Player 1 Total Score: 5; Player 2 Total Score: 4" |
   | Program will alternate turns between players. |  |  |
   | Program will display an error when a player attempts to "roll the die" when it is not their turn. |  |  |
   | Program will add each successive randomly generated number to a player's score when it is their turn. |  |  |
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // Business Logic:
 
-function Player(totalScore, turnScore) {
+function Player(totalScore, turnScore, turnStatus) {
   this.totalScore = totalScore;
   this.turnScore = turnScore;
+  this.turnStatus = turnStatus;
 }
 
 Player.prototype.accumulateTurnScore = function(turnScore, roll) {
@@ -27,23 +28,38 @@ Player.prototype.accumulateTotalScore = function() {
 // UI Logic:
 
 $(document).ready(function() {
-  let player1 = new Player(0, 0);
-  
+  let player1 = new Player(0, 0, true);
+  let player2 = new Player(0, 0, false);
   let turnScore = 0;
   let totalScore = 0;
-  $("button#number-button").click(function(event) {
+  $("button#player1-roll").click(function(event) {
     event.preventDefault();
     let roll = randomNumber(1, 7);
     player1.accumulateTurnScore(turnScore, roll);
-    $("p#number").text(roll + " " + player1.turnScore);
+    $("p#player1-roll-result").text(roll + " " + player1.turnScore);
   });
-  $("button#hold-button").click(function(event) {
+  $("button#player1-hold").click(function(event) {
     event.preventDefault();
     player1.accumulateTotalScore();
-    $("p#total-score").text(player1.totalScore);
-    $("p#number").text(player1.turnScore);
+    $("p#player1-total-score").text(player1.totalScore);
+    $("p#player1-roll-result").text(player1.turnScore);
     if (player1.totalScore >= 100) {
       alert("Player 1 is the winner!");
+    }
+  });
+  $("button#player2-roll").click(function(event) {
+    event.preventDefault();
+    let roll = randomNumber(1, 7);
+    player2.accumulateTurnScore(turnScore, roll);
+    $("p#player2-roll-result").text(roll + " " + player2.turnScore);
+  });
+  $("button#player2-hold").click(function(event) {
+    event.preventDefault();
+    player2.accumulateTotalScore();
+    $("p#player2-total-score").text(player2.totalScore);
+    $("p#player2-roll-result").text(player2.turnScore);
+    if (player2.totalScore >= 100) {
+      alert("Player 2 is the winner!");
     }
   });
 });
